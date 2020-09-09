7b20cdc5bcd4242fa34c2bc7a22f8819f3435400
EMERIC237 <bestvemeric@gmail.com>
2020-09-09 01:13:02 -0400

    from binary to decimal or from decimal to binary

---
+++ convert.py
@@ -0,0 +1,39 @@
+#utf-8 encode
+#/usr/env/python 3
+
+def dec_to_bin(num):
+
+    if num > 1:
+        dec_to_bin(num//2)
+    print(num%2, end='')
+
+
+
+def bin_to_dec(num):
+    dec_value = 0
+    s_num = str(num)
+    e=len(s_num)-1
+    for i in s_num:
+        dec_value = (int(i))*(2**e)+dec_value
+        e-=1
+    print(dec_value)
+
+def main():
+	answer = input("DO you one to move from decimal to binary\nor from binary to decimal\nchoose 'one' for the first option and 'two' for the second option " )
+	if answer == "one":
+		print("well choice")
+		print("You choosed from decimal to binary")
+		number=int(input("write the value you want to convert"))
+		dec_to_bin(number)
+	elif answer == "two":
+		print("well choice")
+		print("You choosed from binary to decimal")
+		number=int(input("write the value you want to convert"))
+		bin_to_dec(number) 
+	else:
+		print("you choosed the worng choice\nPlease,choose 'one' for option 1 or 'two' for option2" )
+		main()
+
+
+if __name__=="__main__":
+	main()
